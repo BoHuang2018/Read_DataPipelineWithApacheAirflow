71cc0887f81b5fc0fac5cd75fedbe6ce6620a2ca
BoHuang2018 <tigerhuangbo@icloud.com>
2022-02-26 17:40:20 +0100

    add .sh file

---
+++ scripts/airflow-in-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+
+SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+
+docker run \
+-ti \
+-p 8080:8080 \
+-v ${SCRIPT_DIR}/../dags/download_rocket_launches.py:/opt/airflow/dags/download_rocket_launches.py \
+--name airflow
+--entrypoint=/bin/bash \
+apache/airflow:2.0.0-python3.9 \
+-c '( \
+airflow db init && \
+airflow users create --username admin --password admin --firstname Anonymous --lastname Admin --role Admin --email admin@example.org \
+); \
+airflow webserver & \
+airflow scheduler \
+'
