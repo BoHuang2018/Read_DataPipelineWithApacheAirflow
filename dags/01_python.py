e6ea4c3666fa0e0408ef3ae3f55a0c41a61ea9c8
BoHuang2018 <tigerhuangbo@icloud.com>
2022-03-28 21:30:52 +0200

    read progression

--- dags/01_python.py
+++ dags/01_python.py
@@ -108,4 +108,24 @@ with DAG(
     )
 
     def _rank_movies(templates_dict, min_ratings=2, **_):
-        # TODO: read progression resume from here
+        input_path = templates_dict["input_path"]
+        output_path = templates_dict["output_path"]
+
+        ratings = pd.read_json(input_path)
+        ranking = rank_movies_by_rating(ratings, min_ratings=min_ratings)
+
+        output_dir = os.path.dirname(output_path)
+        os.makedirs(output_dir, exist_ok=True)
+
+        ranking.to_csv(output_path, index=True)
+
+    rank_movies = PythonOperator(
+        task_id="rank_movies",
+        python_callable=_rank_movies,
+        templates_dict={
+            "input_path": "/data/python/ratings/{{ds}}.json",
+            "output_path": "/data/python/ratings/{{ds}}.csv",
+        },
+    )
+
+    fetch_ratings >> rank_movies
