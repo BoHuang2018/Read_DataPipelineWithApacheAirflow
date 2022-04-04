4dcd1255aa90a9775d3055419d0b7b7bc4fc79ca
BoHuang2018 <tigerhuangbo@icloud.com>
2022-04-04 09:49:08 +0200

    read and refactoring progression, TODO hint to resume, with newly created files

---
+++ docker/movielens-api/api_services/sorted_rating_sample_from_csv_class.py
@@ -0,0 +1,43 @@
+import logging
+import pandas as pd
+
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class SortedRatingsSampleFromCSV:
+    """
+    This class is to generate a sorted dataframe as sample through the following steps
+    1. Read ratings from a csv file and generate pandas DataFrame
+    2. Generate sorted sample of ratings in format of pandas DataFrame
+
+    :attr: csv_file_path: path of csv file containing ratings
+    :attr: number_of_samples: number of samples will be generated
+    :attr: random_state: random seed to reproduce random sample
+    :attr: sort_values: which columns the sorting will be implemented by
+    """
+    csv_file_path: str
+    number_of_samples: int
+    random_state: int
+    sort_values: list
+
+    def _read_ratings_from_csv(self) -> pd.DataFrame:
+        """
+        Step 1: Read ratings from a csv file and generate pandas DataFrame
+        """
+        try:
+            ratings_from_csv = pd.read_csv(self.csv_file_path)
+        except Exception as e:
+            logging.error(f"failed to read ratings from csv file in path: f{self.csv_file_path}")
+            raise e
+        return ratings_from_csv
+
+    @property
+    def generate_sorted_ratings_sample(self) -> pd.DataFrame:
+        """
+        Generate sorted sample of ratings in format of pandas DataFrame
+        """
+        ratings_from_csv = self._read_ratings_from_csv()
+        ratings_sample = ratings_from_csv.sample(n=self.number_of_samples, random_state=self.random_state)
+        sorted_ratings_sample = ratings_sample.sort_values(by=self.sort_values)
+        return sorted_ratings_sample
