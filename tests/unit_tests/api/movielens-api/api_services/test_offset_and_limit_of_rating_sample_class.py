ebf21816b2bc36d7dddc537300f497f30293e390
BoHuang2018 <tigerhuangbo@icloud.com>
2022-04-12 15:36:23 +0200

    Added first unit test for class OffsetAndLimitOfRatingSample.get_arg_offset_of_samples_value

--- tests/unit_tests/api/movielens-api/api_services/test_offset_and_limit_of_rating_sample_class.py
+++ tests/unit_tests/api/movielens-api/api_services/test_offset_and_limit_of_rating_sample_class.py
@@ -1,3 +1,27 @@
 from unittest import TestCase
 
-from api.movielens-api.api_s
+import flask
+from api.movielens_api.api_services.offset_and_limit_of_rating_sample_class import OffsetAndLimitOfRatingSample
+
+
+class TestOffsetAndLimitOfRatingSample(TestCase):
+    def setUp(self):
+        arg_offset_of_samples_key_test = "offset"
+        arg_offset_of_samples_default_value = 0
+        arg_limit_of_samples_key = "limit"
+        arg_limit_of_samples_default_value = 100
+
+        self.offset_and_limit_of_rating_sample = \
+            OffsetAndLimitOfRatingSample(
+                arg_offset_of_samples_key_test,
+                arg_offset_of_samples_default_value,
+                arg_limit_of_samples_key,
+                arg_limit_of_samples_default_value
+            )
+
+
+class TestOffsetAndLimitOfRatingSampleMethodGetArgOffsetOfSamplesValue(TestOffsetAndLimitOfRatingSample):
+    def test_get_arg_offset_of_samples_value(self):
+        app = flask.Flask(__name__)
+        with app.test_request_context('/ratings?offset=100'):
+            self.assertEqual(100, self.offset_and_limit_of_rating_sample.get_arg_offset_of_samples_value)
