74f38f6509eab5657dcff6860a90e1bb12d24dfe
BoHuang2018 <tigerhuangbo@icloud.com>
2022-04-12 21:51:35 +0200

    Unit tests progression

--- tests/unit_tests/api/movielens-api/api_services/test_offset_and_limit_of_rating_sample_class.py
+++ tests/unit_tests/api/movielens-api/api_services/test_offset_and_limit_of_rating_sample_class.py
@@ -20,8 +20,54 @@ class TestOffsetAndLimitOfRatingSample(TestCase):
             )
 
 
+app = flask.Flask(__name__)
+
+
 class TestOffsetAndLimitOfRatingSampleMethodGetArgOffsetOfSamplesValue(TestOffsetAndLimitOfRatingSample):
     def test_get_arg_offset_of_samples_value(self):
-        app = flask.Flask(__name__)
+        """
+        Test the method with given argument 'offset'
+        """
         with app.test_request_context('/ratings?offset=100'):
             self.assertEqual(100, self.offset_and_limit_of_rating_sample.get_arg_offset_of_samples_value)
+        with app.test_request_context('/ratings?offset=100&limit=1000'):
+            self.assertEqual(100, self.offset_and_limit_of_rating_sample.get_arg_offset_of_samples_value)
+        """
+        Test the method without given argument 'offset', it should return default value, 0
+        """
+        with app.test_request_context('/ratings'):
+            self.assertEqual(0, self.offset_and_limit_of_rating_sample.get_arg_offset_of_samples_value)
+        with app.test_request_context('/ratings?limit=1000'):
+            self.assertEqual(0, self.offset_and_limit_of_rating_sample.get_arg_offset_of_samples_value)
+
+    def test_get_arg_limit_of_samples_value_with_given_argument(self):
+        """
+        Test the method with given argument 'limit'
+        """
+        with app.test_request_context('/ratings?limit=101'):
+            self.assertEqual(101, self.offset_and_limit_of_rating_sample.get_arg_limit_of_samples_value)
+        with app.test_request_context('/ratings?limit=101&offset=100'):
+            self.assertEqual(101, self.offset_and_limit_of_rating_sample.get_arg_limit_of_samples_value)
+        """
+        Test the method without given argument 'offset', it should return default value, 100
+        """
+        with app.test_request_context('/ratings'):
+            self.assertEqual(100, self.offset_and_limit_of_rating_sample.get_arg_limit_of_samples_value)
+        with app.test_request_context('/ratings?offset=100'):
+            self.assertEqual(100, self.offset_and_limit_of_rating_sample.get_arg_limit_of_samples_value)
+
+
+class TestOffsetAndLimitOfRatingSampleWithWrongAttributes(TestOffsetAndLimitOfRatingSample):
+    """
+    Test the two @property methods will raise error when
+    1. value of argument, offset, cannot be converted to integer from string
+    2. value of argument, limit, cannot be converted to integer from string
+    """
+    no_integer_values = ['1.1', '1.0', 'abc', 'True', 'False', '@#$123abc', '...']
+
+    def test_get_arg_offset_of_samples_value(self):
+        for v in self.no_integer_values:
+            with self.assertRaises(Exception) as exception_context:
+                with app.test_request_context(f'/ratings?offset={v}'):
+                    self.assertEqual(100, self.offset_and_limit_of_rating_sample.get_arg_offset_of_samples_value)
+            self.assertRaises(Exception, exception_context)
