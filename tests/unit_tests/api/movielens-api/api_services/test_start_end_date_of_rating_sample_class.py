4cd078f948cb32a545e8b5b1b20943907064f381
BoHuang2018 <tigerhuangbo@icloud.com>
2022-04-13 09:13:19 +0200

    started adding unit test to class StartEndDateOfRatingSample

---
+++ tests/unit_tests/api/movielens-api/api_services/test_start_end_date_of_rating_sample_class.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+
+class TestStartEndDateOfRatingSample(TestCase):
+    def setUp(self):
+        arg_start_date_key_test = "start_date"
+        arg_start_date_default_value = '2019-01-01'  # (year-month-day)
