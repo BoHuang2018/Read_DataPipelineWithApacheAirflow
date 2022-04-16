bd07dd08a857b7eda5d894b764fd4f21ea985002
BoHuang2018 <tigerhuangbo@icloud.com>
2022-04-16 13:03:01 +0200

    rename file for test from 'ratings_copy' to 'ratings_copy_for_test'

--- tests/unit_tests/api/movielens-api/api_services/test_sorted_rating_sample_from_csv_class.py
+++ tests/unit_tests/api/movielens-api/api_services/test_sorted_rating_sample_from_csv_class.py
@@ -1,4 +1,18 @@
 from unittest import TestCase
 
+from api.movielens_api.api_services.sorted_rating_sample_from_csv_class import SortedRatingsSampleFromCSV
 
-class SortedRatingsSampleFromCSV(TestCase):
+
+class TestSortedRatingsSampleFromCSV(TestCase):
+    def setUp(self):
+        csv_file_path = "../ratings_copy_for_test.csv"
+        number_of_samples = 100
+        random_state = 0
+        sort_values = ["timestamp", "userId", "movieId"]
+
+        self.sorted_rating_sample_from_csv = SortedRatingsSampleFromCSV(
+            csv_file_path=csv_file_path,
+            number_of_samples=number_of_samples,
+            random_state=random_state,
+            sort_values=sort_values
+        )
