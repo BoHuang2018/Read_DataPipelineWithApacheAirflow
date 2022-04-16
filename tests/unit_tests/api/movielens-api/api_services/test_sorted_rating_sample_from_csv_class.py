fbdd8c3f7a0f45df3f4d1bddee5f5eb278980554
BoHuang2018 <tigerhuangbo@icloud.com>
2022-04-16 14:29:59 +0200

    Finished unit tests with proper attributes

--- tests/unit_tests/api/movielens-api/api_services/test_sorted_rating_sample_from_csv_class.py
+++ tests/unit_tests/api/movielens-api/api_services/test_sorted_rating_sample_from_csv_class.py
@@ -1,5 +1,8 @@
+import logging
+
 import pandas as pd
 from unittest import TestCase
+
 from api.movielens_api.api_services.sorted_rating_sample_from_csv_class import SortedRatingsSampleFromCSV
 
 
@@ -25,3 +28,22 @@ class TestSortedRatingsSampleFromCSVWithProperAttributes(TestSortedRatingsSample
         """
         ratings_from_test_csv = pd.read_csv("../ratings_copy_for_test.csv")
         self.assertTrue(ratings_from_test_csv.equals(self.sorted_rating_sample_from_csv._read_ratings_from_csv()))
+
+    def test_generate_sorted_ratings_sample(self):
+        """
+        Test the method 'generate_sorted_ratings_sample', with test csv file 'ratings_copy_for_test.csv',
+        and other proper attributes:
+            number_of_samples = 100
+            random_state = 0
+            sort_values = ["timestamp", "userId", "movieId"]
+        """
+        sorted_ratings_sample_1 = self.sorted_rating_sample_from_csv.generate_sorted_ratings_sample
+        sorted_ratings_sample_2 = self.sorted_rating_sample_from_csv.generate_sorted_ratings_sample
+        # Both two samples are in type of Dataframe
+        self.assertTrue(isinstance(sorted_ratings_sample_1, pd.DataFrame))
+        self.assertTrue(isinstance(sorted_ratings_sample_2, pd.DataFrame))
+        # Each of samples has 100 rows
+        self.assertEqual(sorted_ratings_sample_1.shape[0], 100)
+        self.assertEqual(sorted_ratings_sample_2.shape[0], 100)
+        # The two sample are identical because of a common random_state and columns for sorting
+        self.assertTrue(sorted_ratings_sample_1.equals(sorted_ratings_sample_2))
