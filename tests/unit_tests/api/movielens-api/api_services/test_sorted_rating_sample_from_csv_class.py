d37fe64cf18005be964d0bf6807f4d2c1366f305
BoHuang2018 <tigerhuangbo@icloud.com>
2022-04-16 13:26:24 +0200

    added unit test for method _read_ratings_from_csv of class SortedRatingsSampleFromCSV

--- tests/unit_tests/api/movielens-api/api_services/test_sorted_rating_sample_from_csv_class.py
+++ tests/unit_tests/api/movielens-api/api_services/test_sorted_rating_sample_from_csv_class.py
@@ -1,5 +1,5 @@
+import pandas as pd
 from unittest import TestCase
-
 from api.movielens_api.api_services.sorted_rating_sample_from_csv_class import SortedRatingsSampleFromCSV
 
 
@@ -16,3 +16,12 @@ class TestSortedRatingsSampleFromCSV(TestCase):
             random_state=random_state,
             sort_values=sort_values
         )
+
+
+class TestSortedRatingsSampleFromCSVWithProperAttributes(TestSortedRatingsSampleFromCSV):
+    def test_read_ratings_from_csv(self):
+        """
+        Test the method '_read_ratings_from_csv', with path to data file 'ratings_copy_for_test.csv'
+        """
+        ratings_from_test_csv = pd.read_csv("../ratings_copy_for_test.csv")
+        self.assertTrue(ratings_from_test_csv.equals(self.sorted_rating_sample_from_csv._read_ratings_from_csv()))
